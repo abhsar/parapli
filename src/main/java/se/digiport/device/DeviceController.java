6f909e7e9f8c456301f8f24be35d9f670d6ec314
abhsar <abhinay.sarkate.01@gmail.com>
2026-05-28 13:38:17 +0200

    Controller to map to correct Endpoints

--- src/main/java/se/digiport/device/DeviceController.java
+++ src/main/java/se/digiport/device/DeviceController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -12,16 +13,17 @@ import java.io.InputStream;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class DeviceController {
 
-    @GetMapping("/api/devices")
+    @GetMapping("/devices")
     public List<Device> getDevices() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         InputStream inputStream = getClass().getResourceAsStream("/data.json");
         return objectMapper.readValue(inputStream, new TypeReference<List<Device>>() {});
     }
 
-    @GetMapping("/api/devices/{id}")
+    @GetMapping("/devices/{id}")
     public ResponseEntity<Device> getDeviceById(@PathVariable String id) throws IOException {
         List<Device> devices = getDevices();
         return devices.stream()
